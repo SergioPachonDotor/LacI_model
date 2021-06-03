3bb3c4a24fab0e0114d761772245425eb223cb98
Sergio Pachon Dotor <sap9827@gmail.com>
2021-06-03 03:00:44 -0500

    V 0.6.1

--- .code_lab/gillespie_lab3.py
+++ .code_lab/gillespie_lab3.py
@@ -1,98 +1,168 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
-import seaborn as sns
-import pandas as pd 
-
-from tqdm import tqdm
-from numpy.random import rand
+import copy
+import warnings
 
 class Gillespie:
-    def __init__(self, tmax=10, species = dict, propensities = dict, reactions= dict):
-        self.sampling_time = 0.001          # Sampling Time
-        self.time = 0                       # Iitial Time
-        self.reference_time = 0             # Reference Time
-        self.tmax = tmax                    # Total Time
-        self.propensities = propensities    # {Reaction: Propensity}
-        self.species = species              # {Species: Quantity}
-        self.reactions = reactions          # {Reaction: Species}
+
+    def __init__(self, tmax=10, sampling_time = 0.01,reaction_model = object):
+        self.sampling_time = sampling_time      # Sampling Time
+        self.time = 0                           # Iitial Time
+        self.reference_time = 0                 # Reference Time
+        self.tmax = tmax                        # Total Time
+        self._reaction_model_copy = copy.copy(reaction_model)
+        self.reaction_model = reaction_model    # Object of reaction model
+        self.tau = 0
+        self.tarr = [0]
+        self.protarr = [0]
 
     def simulate(self):
         """ 
             Performs Cesar Nieto et-al Gillespie algorithm
             for chemical reactions simulations.
         """
+
         while self.time < self.tmax:
 
-            reactions_list = list(self.propensities.keys())
-            propenisties_list = list(self.propensities.values())
+            propensities = self.reaction_model.calculate_propensities()
+            propenisties_list = list(propensities.values())
+            possible_reactions = None
 
-            reaction_times = lambda propensity: -(1/(propensity)) * np.log(rand())
-            calculated_propensities = map(reaction_times, propenisties_list)
+            warnings.simplefilter("ignore")
+            reaction_times = lambda propensity, random_num: -(1/propensity) * np.log(random_num)
+            random_array = [np.random.rand() for k in range(len(propenisties_list))]
 
-            tau = np.min(propenisties_list)     # tau time
-            q = np.argmin(propenisties_list)    # Reaction that occurs
+            calculated_propensities = list(map(reaction_times, propenisties_list, random_array))
+            reactions_dict = dict(enumerate(list(self.reaction_model.show_reactions().keys())))
+            
+            self.tau = np.min(calculated_propensities)     # tau time
+            q = np.argmin(calculated_propensities)    # Reaction that occurs
 
-            if self.time + tau < self.reference_time:
-                pass
+            if self.time + self.tau < self.reference_time:
+                possible_reactions = self.reaction_model.show_q()[reactions_dict[q]]
 
-                self.time += tau
+                create_species = possible_reactions['create']
+                [self.reaction_model.create(name=species) for species in create_species if species != None]
+
+                destroy_species = possible_reactions['destroy']
+                [self.reaction_model.destroy(name=species) for species in destroy_species if species != None]
+
+
+                self.time += self.tau
                 
             else:
                 self.time = self.reference_time  
 
-                # Save Data
-                # tarray.append(t)
-                # protarr.append(proteins)
+                self.tarr.append(self.time)
+                self.protarr.append(self.reaction_model.show_species()['protein'])
 
-                self.reference_time += self.sampling_time   
+                self.reference_time += self.sampling_time
+        
+        plt.plot(self.tarr, self.protarr)
+        plt.savefig('test.jpg')
+        self.reaction_model = self._reaction_model_copy
 
-            reactions_activity = dict(zip(reactions_list, calculated_propensities))
+class ReactionModel:
+    
+    def __init__(self, reactions=dict,species=dict, propensities=dict, q=dict):
+        self.reactions = reactions
+        self.species = species 
+        self.propensities = propensities
+        self.q = q
+    
+    def show_species(self):
+        return self.species
 
-        print(reactions_activity)
+    def show_reactions(self):
+        return self.reactions
+
+    def show_propensities(self):
+        return self.propensities
+    
+    def show_q(self):
+        return self.q
+    
+    def calculate_propensities(self):
+        
+        complete_propensity = list(self.reactions.values())
+        reaction_names = self.reactions.keys()
+        species_copy = copy.copy(self.species)
+        species_copy.update(self.propensities)
+
+        species_calculus = []
+
+        for i in range(len(complete_propensity)):
+            propensities_array = []
+            species_calculus.append(propensities_array)
+
+            for k in complete_propensity[i]:
+                propensities_array.append(species_copy[k])
+                
+        propensities_result = list(map(np.prod, species_calculus))
+        
+        return dict(zip(reaction_names, propensities_result))
+    
+    def create(self, name=str):
+        self.species[name] += 1
+    
+    def destroy(self, name=str):
+        self.species[name] -= 1
+    
+    def set_q(self):
+        pass
 
 if __name__ == '__main__':
-    
-    
+
     species = {
                 'dna': 1, 
                 'rna': 0, 
                 'protein': 0, 
                 'complex': 0
-              }
-
-    reactions = {
-                'create': 1,
-                'destroy': 1
                 }
 
     propensities = {
-                'transcription':       0.005, 
-                'translation':         0.167, 
-                'mrna_degradation':    np.log(2)/150, 
-                'protein_degradation': np.log(2)/3600,
-                'complex_creation':    0.00001,
-                'complex_degradation': 0.005
+                    'trc_c':    0.3, 
+                    'trl_c':    10.02, 
+                    'pdeg_d':   np.log(2)/2.5, 
+                    'cmplx_c':  np.log(2)/30,
+                    'cmplx_d':  0.3
                 }
 
-    print(list(reactions.values()))
-    # sim_3 = Gillespie(tmax=10, species = species, propensities= propensities, reactions= reactions)
-    # sim_3.simulate()
+    reactions = {
+                'transcription': ['trc_c', 'dna'], 
+                'translation':   ['trl_c', 'rna'], 
+                'degradation':   ['pdeg_d', 'protein'], 
+                'complex_c':     ['cmplx_c', 'dna', 'protein'],
+                'complex_d':     ['cmplx_d', 'complex']
+                }
 
+    q = {
+            'transcription': {'create': ['rna'],            'destroy': [None]}, 
+            'translation':   {'create': ['protein'],        'destroy': [None]}, 
+            'degradation':   {'create': [None],             'destroy': ['protein']}, 
+            'complex_c':     {'create': ['complex'],        'destroy': ['dna', 'protein']},
+            'complex_d':     {'create': ['dna', 'protein'], 'destroy': ['complex']}
+        }  
 
-class Reaction:
+    model = ReactionModel(reactions=reactions, species=species, propensities=propensities, q=q)
+    Gillespie(tmax=720, sampling_time=0.1,reaction_model = model).simulate(); 
 
-    def __init__(self, name=str, species=list, propensities=list, action=str):
-        self.name = name
-        self.species = species
-        self.propensities = propensities
-        self.action = action
+    species_2 = {
+                    'protein': 0
+                    }
 
-    def model(self):
-        reaction_model = True
-        return reaction_model
+    propensities_2 = {
+                        'kr': 100, 
+                        'gamma':10
+                        }
+    reactions_2 = {
+                    'translation':['kr'], 
+                    'degradation':['gamma', 'protein']
+                    }
+    q_2 = {
+        'translation': {'create': ['protein'],  'destroy': [None]}, 
+        'degradation': {'create': [None],       'destroy': ['protein']}
+        }
 
-    def birth(self):
-        return
-    
-    def death(self):
-        return
+    # model_2 = ReactionModel(reactions=reactions_2, species=species_2, propensities=propensities_2, q=q_2)
+    # Gillespie(tmax=10, sampling_time=0.01, reaction_model=model_2).simulate()
